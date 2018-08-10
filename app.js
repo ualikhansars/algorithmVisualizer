9c959c173795238768a55dfbce9ee53f1471e263
ualikhansars <valikhan754@gmail.com>
2018-08-10 11:58:11 -0400

    create dataStructures and algorithms routes

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var dataStructures = require('./routes/ds');
+var sortingAlgorithms = require('./routes/sorting');
+var searchingAlgorithms = require('./routes/searching');
 
 var app = express();
 
@@ -24,7 +26,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/dataStructures', dataStructures);
+app.use('/sortingAlgorithms', sortingAlgorithms);
+app.use('/searchingAlgorithms', searchingAlgorithms);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
