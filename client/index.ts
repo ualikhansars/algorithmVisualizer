4eafd57a49e46c9ed36ee7224a3edfbcc6d0fa7c
ualikhansars <valikhan754@gmail.com>
2018-08-15 11:53:32 -0400

    set up webpack

---
+++ client/index.ts
@@ -0,0 +1,8 @@
+export const canvas:any = document.getElementById('testCanvas');
+
+if(canvas) {
+    canvas.width = 1000;
+    canvas.height = 600;
+    canvas.style.border = "7px solid #000";
+}
+
