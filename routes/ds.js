adddfe1e7108eab3df842a2faebe85595aa97c9f
ualikhansars <valikhan754@gmail.com>
2018-08-15 12:52:38 -0400

    add sidebar to data structures

--- routes/ds.js
+++ routes/ds.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/main/dataStructures', {});
+  res.render('pages/main/dataStructures', {description: true});
 });
 
 module.exports = router;
