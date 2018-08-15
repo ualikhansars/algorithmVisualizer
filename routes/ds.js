8b22e83d2a04bdd0bda085ea6b6478a6e7db08f1
ualikhansars <valikhan754@gmail.com>
2018-08-15 13:11:10 -0400

    create elementary data structures routes

--- routes/ds.js
+++ routes/ds.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('pages/main/dataStructures', {description: true});
 });
 
+router.get('/queue', function(req, res, next) {
+  res.render('pages/elementaryDS/queue', {description: true});
+});
+
+router.get('/stack', function(req, res, next) {
+  res.render('pages/elementaryDS/stack', {description: true});
+});
+
+router.get('/linkedList', function(req, res, next) {
+  res.render('pages/elementaryDS/linkedList', {description: true});
+});
+
 module.exports = router;
