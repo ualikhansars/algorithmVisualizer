762f4eaee3940090972b984cade71f38441ed1c5
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-09-02 05:58:19 -0700

    create searching algorithms pages

--- routes/searching.js
+++ routes/searching.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/main/searchingAlgorithms', {});
+});
+
+router.get('/BFS', function(req, res, next) {
+  let mode = req.query;
+  res.render('pages/searching/BFS', {mode});
+});
+
+router.get('/DFS', function(req, res, next) {
+  let mode = req.query;
+  res.render('pages/searching/DFS', {mode});
+});
+
+router.get('/branchAndBound', function(req, res, next) {
+  let mode = req.query;
+  res.render('pages/searching/branchAndBound', {mode});
+});
+
+router.get('/hillClimbing', function(req, res, next) {
+  let mode = req.query;
+  res.render('pages/searching/hillClimbing', {mode});
+});
+
+router.get('/dijkstra', function(req, res, next) {
+  let mode = req.query;
+  res.render('pages/searching/dijkstra', {mode});
+});
+
+router.get('/optimal', function(req, res, next) {
+  let mode = req.query;
+  res.render('pages/searching/optimal', {mode});
+});
+
+router.get('/aStar', function(req, res, next) {
+  let mode = req.query;
+  res.render('pages/searching/aStar', {mode});
 });
 
 module.exports = router;
