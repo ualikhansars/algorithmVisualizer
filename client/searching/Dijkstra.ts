5d74c6cf1a7365dd002f3c04e430d36f00cc9de1
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-09-11 09:50:46 -0700

    implement Dijkstra

--- client/searching/Dijkstra.ts
+++ client/searching/Dijkstra.ts
@@ -1,29 +1,30 @@
 import WeightedAdjListGraph from "../dataStructures/graphs/WeightedAdjListGraph";
+import PriorityQueue from "../dataStructures/queues/PriorityQueue";
 
 export const Dijkstra = (G: WeightedAdjListGraph, s: any) => {
-    let distance: any = {};
     let parent: any = {};
+    let distance: any = {};
+    let Q: PriorityQueue = new PriorityQueue();
     for(let v of G.V) {
-        distance[v] = Number.POSITIVE_INFINITY;
         parent[v] = null;
+        Q.insert(v, Number.POSITIVE_INFINITY);
+        distance[v] = Number.POSITIVE_INFINITY;
     }
+    Q.update(s, 0);
     distance[s] = 0;
     let S = [];
-    let Q = G.V;
-    while(Q.length != 0) {
-        let u = extractMin(Q);
+    while(!Q.isEmpty) {
+        let u = Q.extractMin();
         S.push(u);
         for(let v of G.getAdjArray(u)) {
-            relax(u, v);
+            // relax edge
+            let edge = G.getWeightedEdge(u, v);
+            if(distance[v] > distance[u] + edge.weight) {
+                let newWeight = distance[u] + edge.weight;
+                distance[v] = newWeight
+                parent[v] = u;
+                Q.update(v, newWeight); 
+            }
         }
-        
     }
-}
-
-const extractMin = (Q: any[]) => {
-   
-};  
-
-const relax = (u: any, v: any) => {
-
 }
