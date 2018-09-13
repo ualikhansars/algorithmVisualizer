f5b606a6bc84070fc2934bc8d2bfb28254765c69
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-09-13 08:26:01 -0700

    implement Bellman-Ford algorithm

---
+++ client/searching/BellmanFord.ts
@@ -0,0 +1,33 @@
+import WeightedAdjListGraph from "../dataStructures/graphs/WeightedAdjListGraph";
+import PriorityQueue from "../dataStructures/queues/PriorityQueue";
+
+export const BellmanFord = (G: WeightedAdjListGraph, s: any) => {
+    let negativeCycles = false;
+    let parent: any = {};
+    let distance: any = {};
+    for(let v of G.V) {
+        parent[v] = null;
+        distance[v] = Number.POSITIVE_INFINITY;
+    }
+    distance[s] = 0;
+
+    for(let u of G.V) {
+        for(let v of G.getAdjArray(u)) {
+            let edge = G.getWeightedEdge(u, v).key;
+            if(distance[v] > distance[u] + edge.weight) {
+                let newWeight = distance[u] + edge.weight;
+                distance[v] = newWeight
+                parent[v] = u.key;
+            }
+        }
+    }
+
+    for(let u of G.V) {
+        for(let v of G.getAdjArray(u)) {
+            let edge = G.getWeightedEdge(u, v).key;
+            if(distance[v] > distance[u] + edge.weight) {
+               negativeCycles = true;
+            }
+        }    
+    } 
+};
