5b9b7982e275a7b1eba5d38702061eb0d04a9cbc
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-09-28 08:53:17 -0700

    add stack input field

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,8 @@
 import {drawStack} from './visualRepresentation/dataStructures/stack/drawStack';
 import {codeControl} from './visualRepresentation/dataStructures/stack/codeControl';
+import {process} from "./visualRepresentation/dataStructures/stack/process";
  
 //drawBackground('/images/background.png');
 drawStack();
 codeControl();
+process();
---
+++ client/visualRepresentation/dataStructures/stack/process.ts
@@ -0,0 +1,9 @@
+export const process = () => {
+    let input: any = document.getElementById("stackPush");
+    input.addEventListener('click', (e: any) => {
+        let value = '';
+        let element: any = document.getElementById("stackInput");
+        if(element) value = element.value;  
+        console.log('value', value);
+    });
+}
