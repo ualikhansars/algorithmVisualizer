9adb17e0dbaa90c701051d2cba1539251233b69b
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-09-29 09:35:27 -0700

    draw stack indexes

--- client/visualRepresentation/dataStructures/stack/drawStack.ts
+++ client/visualRepresentation/dataStructures/stack/drawStack.ts
@@ -1,5 +1,8 @@
 import {ctx} from './canvas';
-import {stackLayer} from './stackSettings';
+import {
+    indexLayer,
+    stackLayer
+} from './stackSettings';
 import {drawLine} from '../../utils/draw/drawLine';
 
 export const drawStack = (size: number = 10) => {
@@ -21,3 +24,20 @@ export const drawStack = (size: number = 10) => {
         finishLineY = startLineY;
     }
 }
+
+export const drawIndexLayer = (size: number = 10) => {
+    let x = indexLayer.startX;
+    let y = indexLayer.startY;
+    ctx.font = "18px serif";
+    for(let i = 0; i < size; ++i) {
+        let textX = x + (indexLayer.gridWidth / 2);
+        let textY = y + (indexLayer.gridHeight / 2);
+        ctx.fillText(i, textX, textY);
+        y += indexLayer.gridHeight;
+    }
+}
+
+export const drawVisualRepresentation = () => {
+    drawStack();
+    drawIndexLayer();
+} 
