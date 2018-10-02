83b67dfd42d93e32f99918d7fefc739c54ac9ecc
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-10-02 09:46:25 -0700

    add maxLength attribute to push stack button

--- client/visualRepresentation/dataStructures/stack/push.ts
+++ client/visualRepresentation/dataStructures/stack/push.ts
@@ -2,11 +2,14 @@ import StackVisual from './StackVisual';
 
 let stackVisual = new StackVisual();
 export const onStackPush = () => {
-    let input: any = document.getElementById("stackPush");
-    input.addEventListener('click', (e: any) => {
+    let pushButton: any = document.getElementById("stackPush");
+    pushButton.addEventListener('click', (e: any) => {
         let element: any = document.getElementById("stackInput");
-        if(element) {
+        if(element && element.value) {
            stackVisual.push(element.value);
+           element.value = '';
+           pushButton.disabled = true;
+           setTimeout(() => pushButton.disabled = false, 2000);
         }
     });
 }
