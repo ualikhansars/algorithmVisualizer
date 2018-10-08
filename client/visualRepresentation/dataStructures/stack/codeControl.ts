35f2ac8904316673bf4f4fbab1e43df0593f39bc
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-10-08 09:35:19 -0700

    create drawLineNumber method

--- client/visualRepresentation/dataStructures/stack/codeControl.ts
+++ client/visualRepresentation/dataStructures/stack/codeControl.ts
@@ -6,8 +6,8 @@ import Code from '../../utils/code/Code';
 
 let code = new Code(codeCtx);
 export const codeControl = () => {
-    code.codeLine(0, '// this is sample code');
-    code.codeLine(0, 'for(int i = 0; i < 10; ++i) {');
-    code.codeLine(1, 'print(i)');
-    code.codeLine(0, '}');
+    code.codeLine(1, '// this is sample code');
+    code.codeLine(1, 'for(int i = 0; i < 10; ++i) {');
+    code.codeLine(2, 'print(i)');
+    code.codeLine(1, '}');
 }
--- client/visualRepresentation/utils/code/Code.ts
+++ client/visualRepresentation/utils/code/Code.ts
@@ -1,18 +1,30 @@
+import { drawLine } from "../draw/drawLine";
+
 class Code {
     private fontSize: number = 18;
-    private x: number = 80;
-    private y: number = 80;
+    private x: number = 20; 
+    private y: number = 40;
     private lineHeight: number = 30;
     private indentSize: number = 20;
 
     private line: number = 0;
+    private lineNumber: number = 1;
     private ctx: CanvasRenderingContext2D;
 
     constructor(context: CanvasRenderingContext2D) {
         this.ctx = context;
     }
 
+    private drawLineNumber() {
+        let x = this.x;
+        let y = this.y + this.line * this.lineHeight;
+        this.ctx.font = `${this.fontSize}px serif`;
+        this.ctx.fillText(this.lineNumber.toString(), x, y);
+        this.lineNumber++;
+    } 
+
     codeLine(indent: number, code: string) {
+        this.drawLineNumber();
         let x = this.x + indent * this.indentSize;
         let y = this.y + this.line * this.lineHeight;
         this.ctx.font = `${this.fontSize}px serif`;
