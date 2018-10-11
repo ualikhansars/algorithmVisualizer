e1fd9e3dd1d94b1755775a97a82bb31c1ebb9b5d
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-10-11 08:57:37 -0700

    create codeInstruction class

---
+++ client/visualRepresentation/utils/code/CodeInstruction.ts
@@ -0,0 +1,24 @@
+class CodeInstruction {
+    private instructions: any;
+
+    addToInstructions(line: number, nextInstructionLine: number, animation: Function) {
+        let instruction = new InstructionDetails(line, nextInstructionLine, animation);
+        this.instructions[line] = instruction;
+    }
+
+    processCode(line: number) {
+        const instruction = this.instructions[line];
+    }
+}
+
+class InstructionDetails {
+    line: number = 0;
+    nextInstructionLine: number = 1;
+    animation: any;
+
+    constructor(line: number, nextInstructionLine: number, animation: any) {
+        this.line = line;
+        this.nextInstructionLine = nextInstructionLine;
+        this.animation = animation;
+    }
+}
