2fd75039eb576aede22716818553b847164f1343
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-12-02 05:56:15 -0800

    remove drawBackground

--- client/visualRepresentation/common/canvas.ts
+++ client/visualRepresentation/common/canvas.ts
@@ -5,21 +5,24 @@ export let ctx: any;
 export let codeCtx: any;
 
 export const initializeCanvas = (canvasArea: Area, codeControlArea: Area) => {
-    if(canvas) {
-        canvas.width = canvasArea.width;
-        canvas.height = canvasArea.height;
-        canvas.style.border = "7px solid #000";
-        ctx = canvas.getContext('2d');
-    }
-    
-    if(codeCanvas) {
-        codeCanvas.width = codeControlArea.width;
-        codeCanvas.height = codeControlArea.height;
-        codeCanvas.style.border = "7px solid #000";
-        codeCtx = codeCanvas.getContext('2d');
-    }
-    
-    if(container) {
-        container.position = 'relative';
-    }
+    return new Promise(resolve => {
+        if(canvas) {
+            canvas.width = canvasArea.width;
+            canvas.height = canvasArea.height;
+            canvas.style.border = "7px solid #000";
+            ctx = canvas.getContext('2d');
+        }
+        
+        if(codeCanvas) {
+            codeCanvas.width = codeControlArea.width;
+            codeCanvas.height = codeControlArea.height;
+            codeCanvas.style.border = "7px solid #000";
+            codeCtx = codeCanvas.getContext('2d');
+        }
+        
+        if(container) {
+            container.position = 'relative';
+        }
+        resolve();
+    });
 }
--- client/visualRepresentation/config/canvas/canvasLoader.ts
+++
@@ -1,9 +0,0 @@
-export const background: any = document.getElementById('backgroundCanvas');
-export let backgroundCtx: any;
-
-if(background) {
-    background.width = 1000;
-    background.height = 600;
-    background.style.border = "7px solid #000";
-    backgroundCtx = background.getContext('2d');
-}
